304a2c9ffeac8e09d6f9e23567a42371dc1943b1
annastacia <annastaciangina96@gmail.com>
2022-07-26 23:39:54 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./Home";
+import ReportCase from "./components/ReportCase";
+import About from "./components/About";
+import Comment from "./components/Comment";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Home/>
+      <ReportCase/>
+      <About/>
+      <Comment/>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About(){
+    return(
+        <div id ="about">
+            <h2>About Us</h2>
+            <p>Crime Reporting System is to provide an avenue for the residents and police to report and respond to crimes reported through this platform.
+                click on the link to view the steps required to file a complain online.
+            </p>
+        </div>
+    );
+}
+export default About;
